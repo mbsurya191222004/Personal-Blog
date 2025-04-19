64066e13dfd448c926b5cea8a77c43791e959cd1
mbsurya19122004 <mbsurya19122004@gmail.com>
2025-04-20 02:39:29 +0530

    added table blogs

--- backend/BytesOfMe/MainApp/models.py
+++ backend/BytesOfMe/MainApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Blogs(models.Model):
+    blog = models.TextField
+    date = models.DateField
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    
